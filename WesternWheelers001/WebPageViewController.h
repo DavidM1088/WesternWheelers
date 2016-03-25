6d23471928184c89242d346ea843b54349ab5360
david.murphy <davidp.murphy@sbcglobal.net>
2016-03-25 13:12:57 -0700

    fix code warnings

--- WesternWheelers001/WebPageViewController.h
+++ WesternWheelers001/WebPageViewController.h
@@ -4,7 +4,7 @@
 @interface WebPageViewController : UIViewController <UIWebViewDelegate>
 
 @property (nonatomic) CLLocationCoordinate2D coordinate;
-@property (nonatomic, copy) NSString* title;
+//@property (nonatomic, copy) NSString* title;
 @property (strong, nonatomic) IBOutlet UIWebView *rideWebView;
 @property (strong, nonatomic) Ride *ride;
 
